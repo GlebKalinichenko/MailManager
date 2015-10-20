3fe71d2a1b52f8dbd478990bd85412f6025f1fce
GlebKalinichenko <Glebjn@yandex.ua>
2015-10-20 20:43:54 +0400

    Add call async query for get mail from folder inbox

--- app/src/main/java/com/example/gleb/mailmanager/fragments/InboxFragment.java
+++ app/src/main/java/com/example/gleb/mailmanager/fragments/InboxFragment.java
@@ -46,20 +46,20 @@ import javax.mail.search.FlagTerm;
 /**
  * Created by Gleb on 18.10.2015.
  */
-public class InboxFragment extends Fragment {
+public class InboxFragment extends PatternFragment {
     public static final String TAG = "TAG";
-    private RecyclerView rv;
-    private List<MailStructure> mailStructures;
-    private SuperSwipeRefreshLayout swipeRefreshLayout;
-    private ProgressBar progressBar;
-    private TextView textView;
-    private List<String> arrayFrom;
-    private List<String> arrayEmail;
-    private List<String> arraySubject;
-    private List<String> arrayContent;
-    private List<String> arrayDateMail;
-    private String email;
-    private String password;
+//    private RecyclerView rv;
+//    private List<MailStructure> mailStructures;
+//    private SuperSwipeRefreshLayout swipeRefreshLayout;
+//    private ProgressBar progressBar;
+//    private TextView textView;
+//    private List<String> arrayFrom;
+//    private List<String> arrayEmail;
+//    private List<String> arraySubject;
+//    private List<String> arrayContent;
+//    private List<String> arrayDateMail;
+//    private String email;
+//    private String password;
 
     public InboxFragment(String email, String password) {
         this.email = email;
@@ -130,237 +130,128 @@ public class InboxFragment extends Fragment {
         Log.d(TAG, "Host email " + host);
         switch (host){
             case "yandex.ru":
-                new Loader("imap.yandex.ru", email, password).execute();
+                new Loader("imap.yandex.ru", email, password, "INBOX").execute();
             break;
 
             case "yandex.ua":
-                new Loader("imap.yandex.ru", email, password).execute();
+                new Loader("imap.yandex.ru", email, password, "INBOX").execute();
             break;
 
             case "gmail.com":
-                new Loader("imap.googlemail.com", email, password).execute();
+                new Loader("imap.googlemail.com", email, password, "INBOX").execute();
             break;
 
             case "ukr.net":
-                new Loader("imap.ukr.net", email, password).execute();
+                new Loader("imap.ukr.net", email, password, "INBOX").execute();
             break;
 
             case "rambler.ru":
-                new Loader("imap.rambler.ru", email, password).execute();
+                new Loader("imap.rambler.ru", email, password, "INBOX").execute();
             break;
         }
 
         return v;
     }
 
-    /*
-    * Class for async query for server for mails
-    * */
-    public class Loader extends AsyncTask<String, String, String[]> {
-        public String imapHost;
-        public String user;
-        public String password;
-
-        public Loader(String imapHost, String user, String password) {
-            this.imapHost = imapHost;
-            this.user = user;
-            this.password = password;
-        }
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            Properties props = new Properties();
-            props.put("mail.imap.port", 993);
-            props.put("mail.imap.socketFactory.port", 993);
-            props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.imap.socketFactory.fallback", "false");
-            props.setProperty("mail.store.protocol", "imaps");
-
-            try {
-                Session session = Session.getInstance(props, null);
-                Store store = session.getStore();
-                store.connect(imapHost, email, password);
-                Folder inbox = store.getFolder("INBOX");
-                inbox.open(Folder.READ_ONLY);
-                int newMail = inbox.getNewMessageCount();
-                int allMail = inbox.getMessageCount();
-                Log.d(TAG, "New mail " + newMail);
-                Log.d(TAG, "All mail " + allMail);
-
-                FlagTerm ft = new FlagTerm(new Flags(Flags.Flag.USER), false);
-                Message[] messages = inbox.search(ft);
-                Log.d(TAG, "Новые сообщения " + messages.length);
-
-                for (Folder folder : store.getDefaultFolder().list("*")) {
-                    Log.d(TAG, "Новые сообщения " + folder.getFullName());
-                }
-                arrayFrom = new ArrayList<>();
-                arrayEmail = new ArrayList<>();
-                arraySubject = new ArrayList<>();
-                arrayContent = new ArrayList<>();
-                arrayDateMail = new ArrayList<>();
-
-                for (int i = 0; i < messages.length; i++) {
-                    Address[] in = messages[i].getFrom();
-                    for (Address address : in) {
-                        System.out.println("FROM:" + address.toString());
-                        //add email of sender of mail
-                        String emailValue = address.toString().substring(address.toString().indexOf("<"), address.toString().indexOf(">") + 1);
-                        arrayEmail.add(emailValue);
-                        //add name of sender of mail;
-                        String nameValue = address.toString().substring(0, address.toString().indexOf("<"));
-                        arrayFrom.add(nameValue);
-                    }
-
-                    Object content = messages[i].getContent();
-
-                    //mail content only string values or mail content images with different string values
-                    if (content instanceof String) {
-                        String body = (String) content;
-                        Log.d(TAG, "SENT DATE String: " + messages[i].getSentDate());
-                        //add date of mail
-                        arrayDateMail.add(String.valueOf(messages[i].getSentDate().getDate()) + "." + (messages[i].getSentDate().getMonth() + 1) + "." + (messages[i].getSentDate().getYear() % 100));
-                        //add subject of mail
-                        arraySubject.add(messages[i].getSubject());
-                        Log.d(TAG, "SUBJECT String: " + messages[i].getSubject());
-                        arrayContent.add(body);
-                        Log.d(TAG, "Content String " + body);
-                        createMail(arraySubject.get(i), arrayFrom.get(i), arrayEmail.get(i), arrayContent.get(i), arrayDateMail.get(i));
-                    } else if (content instanceof Multipart) {
-                        Multipart mp = (Multipart) content;
-                        BodyPart bp = mp.getBodyPart(0);
-                        arrayDateMail.add(String.valueOf(messages[i].getSentDate().getDate()) + "." + (messages[i].getSentDate().getMonth() + 1) + "." + (messages[i].getSentDate().getYear() % 100));
-                        Log.d(TAG, "SENT DATE Multipart: " + messages[i].getSentDate());
-                        arraySubject.add(messages[i].getSubject());
-                        Log.d(TAG, "SUBJECT Multipart: " + messages[i].getSubject());
-                        arrayContent.add(bp.getContent().toString());
-                        Log.d(TAG, "Content Multipart " + bp.getContent());
-                        createMail(arraySubject.get(i), arrayFrom.get(i), arrayEmail.get(i), arrayContent.get(i), arrayDateMail.get(i));
-                    }
-                }
-
-            } catch (Exception mex) {
-                mex.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String[] value) {
-            mailStructures = new ArrayList<>();
-            mailStructures = readMail(email);
-//            for (int i = 0; i < arrayFrom.size(); i++){
-//                mailStructures.add(new MailStructure(arrayFrom.get(i), arrayEmail.get(i), arraySubject.get(i), arrayContent.get(i), arrayDateMail.get(i)));
+//    /*
+//    * Class for async query for server for mails
+//    * */
+//    public class Loader extends AsyncTask<String, String, String[]> {
+//        public String imapHost;
+//        public String user;
+//        public String password;
+//
+//        public Loader(String imapHost, String user, String password) {
+//            this.imapHost = imapHost;
+//            this.user = user;
+//            this.password = password;
+//        }
+//
+//        @Override
+//        protected String[] doInBackground(String... params) {
+//            Properties props = new Properties();
+//            props.put("mail.imap.port", 993);
+//            props.put("mail.imap.socketFactory.port", 993);
+//            props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//            props.put("mail.imap.socketFactory.fallback", "false");
+//            props.setProperty("mail.store.protocol", "imaps");
+//
+//            try {
+//                Session session = Session.getInstance(props, null);
+//                Store store = session.getStore();
+//                store.connect(imapHost, email, password);
+//                Folder inbox = store.getFolder("INBOX");
+//                inbox.open(Folder.READ_ONLY);
+//                int newMail = inbox.getNewMessageCount();
+//                int allMail = inbox.getMessageCount();
+//                Log.d(TAG, "New mail " + newMail);
+//                Log.d(TAG, "All mail " + allMail);
+//
+//                FlagTerm ft = new FlagTerm(new Flags(Flags.Flag.USER), false);
+//                Message[] messages = inbox.search(ft);
+//                Log.d(TAG, "Новые сообщения " + messages.length);
+//
+//                for (Folder folder : store.getDefaultFolder().list("*")) {
+//                    Log.d(TAG, "Новые сообщения " + folder.getFullName());
+//                }
+//                arrayFrom = new ArrayList<>();
+//                arrayEmail = new ArrayList<>();
+//                arraySubject = new ArrayList<>();
+//                arrayContent = new ArrayList<>();
+//                arrayDateMail = new ArrayList<>();
+//
+//                for (int i = 0; i < messages.length; i++) {
+//                    Address[] in = messages[i].getFrom();
+//                    for (Address address : in) {
+//                        System.out.println("FROM:" + address.toString());
+//                        //add email of sender of mail
+//                        String emailValue = address.toString().substring(address.toString().indexOf("<"), address.toString().indexOf(">") + 1);
+//                        arrayEmail.add(emailValue);
+//                        //add name of sender of mail;
+//                        String nameValue = address.toString().substring(0, address.toString().indexOf("<"));
+//                        arrayFrom.add(nameValue);
+//                    }
+//
+//                    Object content = messages[i].getContent();
+//
+//                    //mail content only string values or mail content images with different string values
+//                    if (content instanceof String) {
+//                        String body = (String) content;
+//                        Log.d(TAG, "SENT DATE String: " + messages[i].getSentDate());
+//                        //add date of mail
+//                        arrayDateMail.add(String.valueOf(messages[i].getSentDate().getDate()) + "." + (messages[i].getSentDate().getMonth() + 1) + "." + (messages[i].getSentDate().getYear() % 100));
+//                        //add subject of mail
+//                        arraySubject.add(messages[i].getSubject());
+//                        Log.d(TAG, "SUBJECT String: " + messages[i].getSubject());
+//                        arrayContent.add(body);
+//                        Log.d(TAG, "Content String " + body);
+//                        createMail(arraySubject.get(i), arrayFrom.get(i), arrayEmail.get(i), arrayContent.get(i), arrayDateMail.get(i), "Inbox", email);
+//                    } else if (content instanceof Multipart) {
+//                        Multipart mp = (Multipart) content;
+//                        BodyPart bp = mp.getBodyPart(0);
+//                        arrayDateMail.add(String.valueOf(messages[i].getSentDate().getDate()) + "." + (messages[i].getSentDate().getMonth() + 1) + "." + (messages[i].getSentDate().getYear() % 100));
+//                        Log.d(TAG, "SENT DATE Multipart: " + messages[i].getSentDate());
+//                        arraySubject.add(messages[i].getSubject());
+//                        Log.d(TAG, "SUBJECT Multipart: " + messages[i].getSubject());
+//                        arrayContent.add(bp.getContent().toString());
+//                        Log.d(TAG, "Content Multipart " + bp.getContent());
+//                        createMail(arraySubject.get(i), arrayFrom.get(i), arrayEmail.get(i), arrayContent.get(i), arrayDateMail.get(i), "Inbox", email);
+//                    }
+//                }
+//
+//            } catch (Exception mex) {
+//                mex.printStackTrace();
 //            }
-            RVAdapter adapter = new RVAdapter(mailStructures);
-            rv.setAdapter(adapter);
-        }
-    }
-
-    /*
-    * Create mail file in user root directory for user
-    * @param String subject        Subject of mail
-    * @param String from           Name of sender of mail
-    * @param String emailMail      Email of sender of mail
-    * @param String date           Send date of mail
-    * @return void
-    * */
-    public void createMail(String subject, String from, String emailMail, String content, String date){
-        if (subject == null){
-            subject = "";
-        }
-        Log.d(TAG, "Subject " + subject + " from " + from + "Content " + content);
-        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + email + "/Inbox/", from + "-" + date + ".txt");
-        try {
-            file.createNewFile();
-            FileWriter fileWriter = new FileWriter(file);
-            //add name of sender in file
-            fileWriter.write(from);
-            fileWriter.append("\n");
-            //add email of sender in file
-            fileWriter.write(emailMail);
-            fileWriter.append("\n");
-            //add date of mail in file
-            fileWriter.write(date);
-            fileWriter.append("\n");
-            //add subject of mail in file
-            fileWriter.write(subject);
-            fileWriter.append("\n");
-            //add content of mail in file
-            fileWriter.write(content);
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
-    * Read mails from user root directory for user
-    * @param String email        Name user of root directory
-    * @return void
-    * */
-    public List<MailStructure> readMail(String email){
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + email + "/Inbox/";
-        Log.d("Files", "Path: " + path);
-        File f = new File(path);
-        File file[] = f.listFiles();
-        String fromMail;
-        String emailMail;
-        String dateMail;
-        String subjectMail;
-        String contentMail;
-        List<MailStructure> mails = new ArrayList<MailStructure>();
-        for (int i = 0; i < file.length; i++) {
-            int value = 0;
-            fromMail = "";
-            emailMail = "";
-            subjectMail = "";
-            contentMail = "";
-            dateMail = "";
-            Log.d("Files", "FileName:" + file[i].getName());
-            try {
-                InputStream instream = new FileInputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + email + "/Inbox/" + file[i].getName());
-                if (instream != null) {
-                    InputStreamReader inputreader = new InputStreamReader(instream);
-                    BufferedReader buffreader = new BufferedReader(inputreader);
-                    String line = "";
-
-                    while (line != null){
-                        line = buffreader.readLine();
-                        switch (value){
-                            case 0:
-                                fromMail = line;
-                            break;
-
-                            case 1:
-                                emailMail = line;
-                            break;
-
-                            case 2:
-                                dateMail = line;
-                            break;
-
-                            case 3:
-                                subjectMail = line;
-                            break;
-
-                            default:
-                                contentMail += line;
-                            break;
-                        }
-                        value += 1;
-                    }
-                    mails.add(new MailStructure(fromMail, emailMail, subjectMail, contentMail, dateMail));
-                    instream.close();
-
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        return mails;
-    }
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String[] value) {
+//            mailStructures = new ArrayList<>();
+//            mailStructures = readMail(email, "Inbox");
+//            RVAdapter adapter = new RVAdapter(mailStructures);
+//            rv.setAdapter(adapter);
+//        }
+//    }
 }
