cba9bde928a6202e4d32cf86a1761a7ad91b8b0d
GlebKalinichenko <Glebjn@yandex.ua>
2015-10-20 20:35:59 +0400

    Show mail from folder draft

---
+++ app/src/main/java/com/example/gleb/mailmanager/fragments/DraftFragment.java
@@ -0,0 +1,113 @@
+package com.example.gleb.mailmanager.fragments;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.gleb.mailmanager.R;
+import com.example.gleb.mailmanager.recyclerview.ItemRecycler;
+import com.example.gleb.mailmanager.swipe.SuperSwipeRefreshLayout;
+
+/**
+ * Created by Gleb on 20.10.2015.
+ */
+public class DraftFragment extends PatternFragment {
+    public static final String TAG = "Tag";
+
+    public DraftFragment(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_mail_manager, container, false);
+        rv=(RecyclerView) v.findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        rv.addOnItemTouchListener(
+                new ItemRecycler.RecyclerItemClickListener(getActivity(), new ItemRecycler.RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Log.d(TAG, "Value" + position);
+                    }
+
+                    @Override
+                    public void onItemLongPress(View childView, int position) {
+
+                    }
+                })
+        );
+        swipeRefreshLayout = (SuperSwipeRefreshLayout) v.findViewById(R.id.swipe_refresh);
+        View child = LayoutInflater.from(swipeRefreshLayout.getContext()).inflate(R.layout.layout_head, null);
+        progressBar = (ProgressBar) child.findViewById(R.id.progressBar);
+        textView = (TextView) child.findViewById(R.id.text_view);
+        swipeRefreshLayout
+                .setOnPullRefreshListener(new SuperSwipeRefreshLayout.OnPullRefreshListener() {
+
+                    @Override
+                    public void onRefresh() {
+                        textView.setText("К вам летит письмо...");
+                        progressBar.setVisibility(View.VISIBLE);
+                        new Handler().postDelayed(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                swipeRefreshLayout.setRefreshing(false);
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }, 2000);
+
+                    }
+
+                    @Override
+                    public void onPullDistance(int distance) {
+                        //myAdapter.updateHeaderHeight(distance);
+                    }
+
+                    @Override
+                    public void onPullEnable(boolean enable) {
+//                        imageView.setVisibility(View.VISIBLE);
+//                        imageView.setRotation(enable ? 180 : 0);
+                    }
+                });
+
+        String host = email.substring(email.lastIndexOf("@") + 1);
+        Log.d(TAG, "Host email " + host);
+        switch (host){
+            case "yandex.ru":
+                new Loader("imap.yandex.ru", email, password, "Черновики").execute();
+                break;
+
+            case "yandex.ua":
+                new Loader("imap.yandex.ru", email, password, "Черновики").execute();
+                break;
+
+            case "gmail.com":
+                new Loader("imap.googlemail.com", email, password, "Черновики").execute();
+                break;
+
+            case "ukr.net":
+                new Loader("imap.ukr.net", email, password, "Черновики").execute();
+                break;
+
+            case "rambler.ru":
+                new Loader("imap.rambler.ru", email, password, "Черновики").execute();
+                break;
+        }
+
+        return v;
+    }
+
+}
