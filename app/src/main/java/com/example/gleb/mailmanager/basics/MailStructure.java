05a2424d651e4563681154f995e7a777708ecced
GlebKalinichenko <Glebjn@yandex.ua>
2015-10-23 18:05:11 +0400

    Add attach file for mail

--- app/src/main/java/com/example/gleb/mailmanager/basics/MailStructure.java
+++ app/src/main/java/com/example/gleb/mailmanager/basics/MailStructure.java
@@ -1,21 +1,36 @@
 package com.example.gleb.mailmanager.basics;
 
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+
 /**
  * Created by Gleb on 18.10.2015.
  */
-public class MailStructure {
+public class MailStructure implements Serializable {
     private String from;
     private String email;
     private String subject;
     private String content;
     private String date;
+    private String[] attachFiles;
 
-    public MailStructure(String from, String email, String subject, String content, String date) {
+    public MailStructure(String from, String email, String subject, String content, String date, String[] attachFiles) {
         this.from = from;
         this.email = email;
         this.subject = subject;
         this.content = content;
         this.date = date;
+        this.attachFiles = attachFiles;
+    }
+
+    public String[] getAttachFiles() {
+        return attachFiles;
+    }
+
+    public void setAttachFiles(String[] attachFiles) {
+        this.attachFiles = attachFiles;
     }
 
     public String getFrom() {
